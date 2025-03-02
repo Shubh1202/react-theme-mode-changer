c4b6e57920fe3d977eaf5c89775f0a2c20900ae7
Shubh1202 <shubhamprajapati1202@gmail.com>
2025-03-02 16:25:18 +0530

    add agent defination

--- src/component/Card.js
+++ src/component/Card.js
@@ -8,15 +8,14 @@ const Card = () => {
             <div className="p-4">
                 <div className="mb-2 flex items-center justify-between">
                     <p className="text-slate-800 dark:text-gray-200 text-xl font-semibold">
-                        What is an AI Agent
+                    What are AI agents?
                     </p>
                     <p className="text-cyan-600 dark:text-cyan-400 text-xl font-semibold">
                         $95.00
                     </p>
                 </div>
                 <p className="text-slate-600 dark:text-gray-400 leading-normal font-light">
-                    With plenty of talk and listen time, voice-activated Siri access, and
-                    an available wireless charging case.
+                An artificial intelligence (AI) agent refers to a system or program that is capable of autonomously performing tasks on behalf of a user or another system by designing its workflow and utilizing available tools.
                 </p>
                 <button className="rounded-md w-full mt-6 bg-cyan-600 dark:bg-cyan-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-cyan-700 focus:shadow-none active:bg-cyan-700 hover:bg-cyan-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
                     Read More
